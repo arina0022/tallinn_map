76f173b5dfa32829b9fb9abfad6a3730f6bb6ff8
arina0022 <arina.aslamova@yandex.ru>
2025-03-03 10:16:42 +0200

    Updated files

--- wintermaintenance/map/urls.py
+++ wintermaintenance/map/urls.py
@@ -4,5 +4,5 @@ from .views import map_view, ready_map,ready_map_hour
 urlpatterns = [
     path('', map_view, name='map_view'),
     path('ready', ready_map, name='ready_map'),
-    path('ready_hour', ready_map_hour, name='ready_map'),
+    path('ready_by_hour', ready_map_hour, name='ready_map'),
 ]
